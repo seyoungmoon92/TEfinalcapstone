03af57868a14f96689b291bbc499558007b191f8
Lucas Schilling <Lucasrschilling@gmail.com>
2022-04-09 11:06:04 -0400

    Dummy Data Added

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -22,4 +22,59 @@ INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULi
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
 
+CREATE TABLE landmarks (
+    landmarkId serial,
+    address varchar(80) not null,
+    description varchar(255),
+    rating int,
+
+    CONSTRAINT pk_landmarks PRIMARY KEY (landmarkId)
+
+);
+
+--CREATE TABLE landmarks_reviews (
+--    landmarkId int,
+--    reviewId int
+--
+--    CONSTRAINT fk_landmarks FOREIGN KEY (landmarkId),
+--    CONSTRAINT fk_reviews FOREIGN KEY (reviewId)
+--);
+--
+--CREATE TABLE reviews (
+--    reviewId serial,
+--    name varchar(50),
+--    title varchar(60),
+--    body varchar(255)
+--
+--    CONSTRAINT pk_reviews PRIMARY KEY (reviewId)
+--);
+--
+--CREATE TABLE itinerary (
+--    user_id int,
+--    landmarkId int
+--
+--    CONSTRAINT FK_user FOREIGN KEY (user_id),
+--    CONSTRAINT FK_landmarks FOREIGN KEY (landmarkId)
+--
+--);
+
+
+
+
+
+
+
+INSERT INTO landmarks (landmarkId, address, description, rating ) VALUES (1, 'test road', 'test description1', 4);
+INSERT INTO landmarks (landmarkId, address, description, rating ) VALUES (2, 'test1 road', 'test description2', 5);
+INSERT INTO landmarks (landmarkId, address, description, rating ) VALUES (3, 'test2 road', 'test description3', 1);
+INSERT INTO landmarks (landmarkId, address, description, rating ) VALUES (4, 'test3 road', 'test description4', 2);
+INSERT INTO landmarks (landmarkId, address, description, rating ) VALUES (5, 'test4 road', 'test description5', 3);
+
+-- UPDATING SEQUENCES SO THERE ARE NO CLASHES WHEN APP RUNS WITH EXISTING KEYS...
+ALTER SEQUENCE landmarks_landmarkId_seq RESTART WITH 100;
+
+
+
+
 COMMIT TRANSACTION;
+
