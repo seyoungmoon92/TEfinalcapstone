f8313903f5b2068897b6389ec9c4d1c88a0ce195
Seyoung Moon <seyoungmoon@gmail.com>
2022-04-11 11:56:16 -0400

    map component added to front end

--- vue/src/main.js
+++ vue/src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router/index'
 import store from './store/index'
 import axios from 'axios'
+import * as VueGoogleMaps from "vue2-google-maps" // Import package
 
 Vue.config.productionTip = false
 
 axios.defaults.baseURL = process.env.VUE_APP_REMOTE_API;
+
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCcrOUiA-7wHhNdkdczzY1PMgTFclDgYAg",
+    libraries: "places"
+  }
+});
 
 new Vue({
   router,
--- vue/src/views/LandmarkSearchView.vue
+++ vue/src/views/LandmarkSearchView.vue
@@ -1,15 +1,32 @@
 <template>
+<div>
   <landmark-search/>
+  <div id="map">
+      <AddGoogleMap />
+   </div>
+</div>
 </template>
 
 <script>
+import AddGoogleMap from "../components/Map.vue";
 import LandmarkSearch from '../components/LandmarkSearch.vue'
 export default {
-  components: { LandmarkSearch },
+  components: { 
+    LandmarkSearch,
+    AddGoogleMap
+   },
 
 }
 </script>
 
-<style>
+<style scoped>
+#map {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000000;
+  margin-top: 50px;
+}
 
 </style>
