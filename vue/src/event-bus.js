5c71552f1da5b6a86c1ca6379eb93186370b7ca0
Joseph Bennett <5njnets@gmail.com>
2022-04-18 13:44:50 -0400

    styled log in and registration page

--- vue/src/event-bus.js
+++ vue/src/event-bus.js
@@ -3,3 +3,16 @@ import Vue from 'vue'
 export const eventBus = new Vue({
   methods: {}
 });
+
+// let $id = (id) => document.getElementById(id);
+// var [login, register, form] = ['login', 'register', 'form'].map(id => $id(id));
+
+// [login, register].map(element => {
+//     element.onclick = function () {
+//         [login, register].map($ele => {
+//             $ele.classList.remove("active");
+//         });
+//         this.classList.add("active");
+//         this.getAttribute("id") === "register"?  form.classList.add("active") : form.classList.remove("active");
+//     }
+// });
