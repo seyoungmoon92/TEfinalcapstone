2c7f47eaff6655f332f0d0cd4d940ca1a09615ec
Philmon Yosief <philmony9@gmail.com>
2022-04-19 09:42:18 -0400

    pull from origin

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -64,28 +64,13 @@ CREATE TABLE photos (
 
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (1, 'Tokyo National Mueseum', '13-9 Uenokoen, Taito City, Tokyo 110-8712, Japan', 'The Tokyo National Museum is an art museum in Ueno Park in the Taito ward of Tokyo, Japan. It is one of the four museums operated by the National Institutes for Cultural Heritage , is considered the oldest national museum in Japan, is the largest art museum in Japan, and is one of the largest art museums in the world.', 4, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (2, 'Ghibli Museum', '1 Chome-1-83 Shimorenjaku, Mitaka, Tokyo 181-0013, Japan', 'Whimsical museum dedicated to the famed animation studio with a play area, theater & rooftop garden.', 5, true);
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 0f067b8bdbb58cf6bed1b9b0e45f5ede185f81c3
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (3, 'Sensoji Temple', '2 Chome-3-1 Asakusa, Taito City, Tokyo 111-0032, Japan', 'Sensō-ji is an ancient Buddhist temple located in Asakusa, Tokyo, Japan. This buddhist temple is the oldest and one of the most significant temples in Tokyo.', 10, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (4, 'Ueno Park', 'Japan, 〒110-0007 Tokyo, Taito City, Uenokoen, 池之端三丁目', 'A paradise-like oasis of green in the heart of busy Tokyo, Ueno Park (Ueno Kōen) is the largest green space and one of the most popular tourist attractions in the city. In addition to its lovely grounds, the park also boasts numerous temples and museums to explore.', 8, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (5, 'Meiji Shrine', '1-1 Yoyogikamizonocho, Shibuya City, Tokyo 151-8557, Japan', 'Dedicated to Emperor Meiji and his wife, Empress Shōken, construction of the splendid Meiji Shrine (Meiji Jingū) began in 1915 and was completed in 1926. Although the original structure was destroyed during WWII, it was rebuilt in 1958 and remains one of Tokyo''s most important religious sites.', 4, true);
-
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (6, 'Tokyo Skytree', '1 Chome-1-2 Oshiage, Sumida City, Tokyo 131-0045, Japan', 'The country''s tallest structure (and the world''s tallest freestanding tower), the Tokyo Skytree opened in 2012 and has quickly become one of the city''s most visited tourist attractions thanks to the incredible panoramic views from its restaurant and observation decks.', 3, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (7, 'Kabuki-za Theatre', '4 Chome-12-15 Ginza, Chuo City, Tokyo 104-0061, Japan', 'Tokyo is home to a number of excellent theaters, none as well known as the historic Kabuki-za Theatre in the city''s busy Ginza district, home to famous traditional Kabuki performances. Based upon a medieval, highly skilled, and often burlesque theatrical form including song and dance, the theater''s performances are as popular among tourists as they are with Japanese-speaking people.', 9, true);
-<<<<<<< HEAD
-
-
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (8, 'Tsukiji Outer Market', '4 Chome-16番2号 Tsukiji, Chuo City, Tokyo 104-0045, Japan', 'Tsukiji Outer Market is Japan’s “Food Town,” where one can encounter all kinds of traditional Japanese foods. A mixture of wholesale and retail shops, along with numerous restaurants, line the streets, and new culinary trends are born here.', 13, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (9, 'Hachiko Statue', '2 Chome-1 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan', 'Chuken Hachiko is a touching and heartbreaking story of the tight bond between a dog and his master. This statue was built in honor of Chuken Hachiko who eternally waited for his deceased master to return to him.', 78, true);
-
-
-=======
-INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (8, 'Tsukiji Outer Market', '4 Chome-16番2号 Tsukiji, Chuo City, Tokyo 104-0045, Japan', 'Tsukiji Outer Market is Japan’s “Food Town,” where one can encounter all kinds of traditional Japanese foods. A mixture of wholesale and retail shops, along with numerous restaurants, line the streets, and new culinary trends are born here.', 13, true);
-INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (9, 'Hachiko Statue', '2 Chome-1 Dogenzaka, Shibuya City, Tokyo 150-0043, Japan', 'Chuken Hachiko is a touching and heartbreaking story of the tight bond between a dog and his master. This statue was built in honor of Chuken Hachiko who eternally waited for his deceased master to return to him.', 78, true);
->>>>>>> 0f067b8bdbb58cf6bed1b9b0e45f5ede185f81c3
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (10, 'Nakamise-dori Street', '1 Chome-36-3 Asakusa, Taito City, Tokyo 111-0032, Japan', 'Get a feel for old-school Tokyo at one of Japan''s oldest shopping streets, Nakamise-dori, which dates back to the 17th century. Most shops in this arcade have been run by the same families for several generations, serving souvenirs, top-class street food and irresistible snacks.', 32, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (11, 'Tokyo Disneyland', '1-1 Maihama, Urayasu, Chiba 279-0031, Japan', 'Tokyo Disneyland is a 115-acre theme park at the Tokyo Disney Resort in Urayasu, Chiba Prefecture, Japan, near Tokyo. It was the first Disney park to be built outside the United States and it opened on April 15, 1983.', 190, true);
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (12, 'Harajuku', 'Jingumae Shibuya City, Tokyo 150-0001 Japan', 'Harajuku is a district in Shibuya, Tokyo, Japan, that is known internationally as a center of Japanese youth culture and fashion. Shopping and dining options include many small, youth-oriented, independent boutiques and cafés, but the neighborhood also attracts many larger international chain stores with high-end luxury merchandisers extensively represented along Omotesando.', 22, true);
@@ -99,17 +84,6 @@ INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive
 INSERT INTO landmarks (landmarkId, name, address, description, rating , isActive) VALUES (20, 'Sengakuji Temple', '2 Chome-11-1 Takanawa, Minato City, Tokyo 108-0074, Japan', 'Sengakuji is a small temple near Shinagawa Station in Tokyo. The temple is famous for its graveyard where the "47 Ronin" (also known as Akoroshi, the "masterless samurai from Ako") are buried.', 23, true);
 
 INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (1,1,'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Tokyo_National_Museum%2C_Honkan_2010.jpg/1200px-Tokyo_National_Museum%2C_Honkan_2010.jpg');
-<<<<<<< HEAD
---
---INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (2,2,'https://image.arrivalguides.com/1230x800/00/8696e6a1843072d16aee646ca33d62ae.jpg');
---INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (3,3,'https://mokolate.com/wp-content/uploads/2016/12/IMG_4687.jpg');
---INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (4,4,'https://th.bing.com/th/id/R.4758d7ef05fb25a24218a3a6a0217c39?rik=DBfhDvaPwBfSJQ&pid=ImgRaw&r=0');
---INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (5,5,'https://cdn7.japanrailpass.com.au/wp-content/uploads/2017/08/Chureito-pagoda-in-foreground-and-mount-fuji-in-background-during-sunrise-time.-Beautiful-nature-and-human-culture.jpg');
---
-
-
-=======
->>>>>>> 0f067b8bdbb58cf6bed1b9b0e45f5ede185f81c3
 INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (2,2,'https://mokolate.com/wp-content/uploads/2016/12/IMG_4687.jpg');
 INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (3,3,'https://cdn.cheapoguides.com/wp-content/uploads/sites/2/2020/05/sensoji-temple-iStock-1083328636-770x514.jpg');
 INSERT INTO photos (photoId, landmarkId, photoUrl) VALUES (4,4, 'https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/03/a0003134/img/basic/a0003134_main.jpg?20200514174020&q=80');
